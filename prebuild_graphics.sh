a6a526c6f14f787940fcf4df8261b7d8afe901d5
Jiří Janoušek <janousek.jiri@gmail.com>
2014-03-02 16:00:07 +0100

    Prebuilt web app icons.

--- prebuild_graphics.sh
+++ prebuild_graphics.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eu # European Union compliance mode
 
 PREFIX="${PREFIX:-.}"
@@ -13,6 +13,21 @@ prebuild_app_icon()
 	rsvg-convert -w $size -h $size \
 	"graphics/nuvola-icon/nuvola-player.${source}.svg" \
 	-o "$directory/nuvolaplayer.png"
+	{ set +x; } 2>/dev/null
+}
+
+prebuild_web_app_icon()
+{
+	source="$1"
+	size=26
+	name="$(basename "$source")"
+	name="${name%.svg}"
+	directory="$PREFIX/data/nuvolaplayer3/web_apps/$name"
+	[ -d "$directory" ] || mkdir -p "$directory"
+	set -x
+	rsvg-convert -w $size -h $size \
+	"${source}" \
+	-o "$directory/icon.png"
 	{ set +x; } 2>/dev/null
 }
 
@@ -34,3 +49,8 @@ for size in 48 64; do prebuild_app_icon orig $size; done
 optimize_svg \
 graphics/nuvola-icon/nuvola-player.orig.svg \
 "$PREFIX/data/icons/hicolor/scalable/apps/nuvolaplayer.svg"
+
+for source in graphics/service-icons/*.svg; do
+	[[ "$source" != *.*.svg ]] || continue
+	prebuild_web_app_icon "$source"
+done
