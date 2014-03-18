8ef2f4fe59e2e8c812aaccbfc287ae2374591fd3
Jiří Janoušek <janousek.jiri@gmail.com>
2014-03-18 20:53:45 +0100

    added gdb aliases

--- set_up_env.sh
+++ set_up_env.sh
@@ -3,9 +3,22 @@ rebuild()
 	./waf distclean configure build "$@"
 }
 
-debug()
+run()
 {
 	./waf -v && LD_LIBRARY_PATH=build XDG_DATA_DIRS=data:/usr/share:/usr/local/share \
 	NUVOLA_WEBKIT_EXTENSION_DIR=build build/nuvolaplayer3 -D "$@"
 
+}
+
+debug()
+{
+	./waf -v && LD_LIBRARY_PATH=build XDG_DATA_DIRS=data:/usr/share:/usr/local/share \
+	NUVOLA_WEBKIT_EXTENSION_DIR=build gdb --args build/nuvolaplayer3 -D "$@"
+}
+
+debug_criticals()
+{
+	./waf -v && LD_LIBRARY_PATH=build XDG_DATA_DIRS=data:/usr/share:/usr/local/share \
+	NUVOLA_WEBKIT_EXTENSION_DIR=build G_DEBUG=fatal-criticals \
+	gdb --args build/nuvolaplayer3 -D "$@"
 }
