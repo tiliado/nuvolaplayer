c86c6622ffd0565300ba8bc9c306689f362fb972
Jiří Janoušek <janousek.jiri@gmail.com>
2014-06-20 13:39:04 +0200

    Add debug_app_runner_criticals command
    It sets up gdbserver for app runner with fatal critical warnings.
    
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- set_up_env.sh
+++ set_up_env.sh
@@ -29,6 +29,13 @@ debug_app_runner()
 	NUVOLA_LIBDIR=build NUVOLA_APP_RUNNER_GDB_SERVER='localhost:9090' build/nuvolaplayer3 -D "$@"
 }
 
+debug_app_runner_criticals()
+{
+	./waf -v && LD_LIBRARY_PATH=build XDG_DATA_DIRS=data:/usr/share:/usr/local/share \
+	NUVOLA_LIBDIR=build G_DEBUG=fatal-criticals NUVOLA_APP_RUNNER_GDB_SERVER='localhost:9090' \
+	build/nuvolaplayer3 -D "$@"
+}
+
 debug_app_runner_join()
 {
 	echo Use "'target remote localhost:9090'"
