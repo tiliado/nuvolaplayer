007ef295d88b72e15e3407cb007fb6141bfce9b5
Jiří Janoušek <janousek.jiri@gmail.com>
2014-07-11 08:01:44 +0200

    Add doc for Nuvola.KeyValueStorage and children
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- src/mainjs/storage.js
+++ src/mainjs/storage.js
@@ -25,43 +25,116 @@
 require("prototype");
 require("signals");
 
+/**
+ * Prototype object to hold key-value mapping
+ */
 var KeyValueStorage = $prototype(null);
 
+/**
+ * Initializes new key-value storage
+ * 
+ * @param Number index    index in storage pool
+ */
 KeyValueStorage.$init = function(index)
 {
 	this.index = index;
 }
 
+/**
+ * Set default value for given key
+ * 
+ * This function should be called only once per key, for example in @link{Core::InitAppRunner} handler.
+ * 
+ * @param String key       the key name
+ * @param Variant value    value of the key
+ * 
+ * ```
+ * WebApp._onInitAppRunner = function(emitter, values, entries)
+ * {
+ *     Nuvola.WebApp._onInitAppRunner.call(this, emitter, values, entries);
+ * 
+ *     var ADDRESS = "app.address";
+ *     // Nuvola.config is a KeyValueStorage
+ *     Nuvola.config.setDefault(ADDRESS, "default");
+ * }
+ * ```
+ */
 KeyValueStorage.setDefault = function(key, value)
 {
 	Nuvola._keyValueStorageSetDefaultValue(this.index, key, value);
 }
 
+/**
+ * Check whether the storage has given key
+ * 
+ * @param String key    storage key name
+ * @return ``false`` if the storage doesn't contain value for the key
+ *     (even if @link{KeyValueStorage.setDefault|default value has been set}),
+ *     ``true`` otherwise
+ */
 KeyValueStorage.hasKey = function(key)
 {
 	return Nuvola._keyValueStorageHasKey(this.index, key);
 }
 
+/**
+ * Get value by key name
+ * 
+ * Note that behavior on a key without an assigned value nor @link{KeyValueStorage.setDefault|the default value}
+ * is undefined - it may return *anything* or throw and error. (The current implementation returns string ``"<UNDEFINED>"``
+ * as it helps to identify unwanted manipulation with ``undefined`` value type.)
+ * 
+ * @param String key    key name
+ * @return value set by @link{KeyValueStorage.set} or @link{KeyValueStorage.setDefault} for given key
+ */
 KeyValueStorage.get = function(key)
 {
 	return Nuvola._keyValueStorageGetValue(this.index, key);
 }
 
+/**
+ * Set value for given key
+ * 
+ * @param String key       key name
+ * @param Variant value    value of given key
+ */
 KeyValueStorage.set = function(key, value)
 {
 	Nuvola._keyValueStorageSetValue(this.index, key, value);
 }
 
+/**
+ * Prototype object to access persistent configuration
+ * 
+ * Note: Use @link{SessionStorage} for temporary data.
+ */
 var ConfigStorage = $prototype(KeyValueStorage, SignalsMixin);
 
+/**
+ * Initializes new ConfigStorage object
+ */
 ConfigStorage.$init = function()
 {
 	KeyValueStorage.$init.call(this, 0);
-	this.registerSignals(["ConfigChanged"]);
+	
+	/**
+	 * Emitted when a configuration key is changed
+	 * 
+	 * @param String key    key name
+	 */
+	this.addSignal("ConfigChanged");
 }
 
+/**
+ * Prototype object of a key-value storage with a lifetime limited to the current session
+ * 
+ * Note: Use @link{SessionStorage} to store persistent data.
+ */
 var SessionStorage = $prototype(KeyValueStorage);
 
+/**
+ * Initializes new SessionStorage object.
+ */
 SessionStorage.$init = function()
 {
 	KeyValueStorage.$init.call(this, 1);
