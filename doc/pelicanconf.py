a586ca1180721a47b8d70cfdbb7c7dcfafab2375
Jiří Janoušek <janousek.jiri@gmail.com>
2014-07-25 08:20:41 +0200

    Lower header level in MarkDown documents
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- doc/pelicanconf.py
+++ doc/pelicanconf.py
@@ -39,15 +39,39 @@ TAGS_SAVE_AS = ''
 RELATIVE_URLS = True
 STATIC_PATHS = ['images']
 
+import re
 from markdown.extensions import Extension
+from markdown.treeprocessors import Treeprocessor
+
 class MarkdownConfigExtension(Extension):
     def extendMarkdown(self, md, md_globals):
         md.safeMode = 'escape'
         md.enable_attributes = False
         md.lazy_ol = False
 
+class LowerHeaderLevelExtension(Extension):
+    def __init__(self, step=1, *args, **kwds):
+        Extension.__init__(self, *args, **kwds)
+        self.step = step
+    
+    def extendMarkdown(self, md, md_globals):
+        md.treeprocessors.add('lowerheaderlevel', LowerHeaderLevelProcessor(self.step), '_end')
+        
+class LowerHeaderLevelProcessor(Treeprocessor):
+    def __init__(self, step):
+        Treeprocessor.__init__(self)
+        self.step = step
+        self.expr = re.compile('h(\d)')
+    
+    def run(self, node):
+        for child in node.getiterator():
+            match = self.expr.match(child.tag)
+            if match:
+                child.tag = 'h{}'.format(min(6, int(match.group(1)) + self.step))
+        return node
+
 MD_EXTENSIONS = [
-    MarkdownConfigExtension(),
+    MarkdownConfigExtension(), LowerHeaderLevelExtension(),
     'extra', 'sane_lists', 'fenced_code', 'codehilite',
     'def_list', 'attr_list', 'abbr', 'admonition'
     ]
