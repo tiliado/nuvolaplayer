412b42c150cb55c7320678054c43b4fb1b564672
Jiří Janoušek <janousek.jiri@gmail.com>
2014-07-30 18:14:57 +0200

    Add prompt prefix to set up env
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- set_up_env.sh
+++ set_up_env.sh
@@ -2,6 +2,10 @@ export NUVOLA_WEB_APPS_DIR="web_apps"
 export DIORITE_LOG_IPC_SERVER="yes"
 export DIORITE_LOG_MESSAGE_SERVER="yes"
 
+prompt_prefix='\[\033[1;33m\]Nuvola\[\033[00m\]'
+[[ "$PS1" = "$prompt_prefix"* ]] || export PS1="$prompt_prefix $PS1"
+unset prompt_prefix
+
 rebuild()
 {
 	./waf distclean configure build "$@"
