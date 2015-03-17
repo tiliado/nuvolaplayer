3e5c88756e321bd7edf0fac5e6e06e268d71f0a7
Jiří Janoušek <janousek.jiri@gmail.com>
2015-03-17 23:34:01 +0100

    Add fedora bootstrap scripts
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

---
+++ tools/fedora21_bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+yum install -y rsync vala-devel python python3 glib2-devel gtk3-devel json-glib-devel \
+ libnotify-devel libarchive-devel webkitgtk4-devel libsoup \
+ intltool gstreamer1-devel \
+ gdb libtool gdb-gdbserver geany
