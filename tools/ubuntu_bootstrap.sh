ebd34d1430470bd41ca294f2a1a4bb01541bff84
Jiří Janoušek <janousek.jiri@gmail.com>
2015-03-18 22:34:30 +0100

    Add ubuntu bootstrap script
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

---
+++ tools/ubuntu_bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+apt-get install -y libglib2.0-dev libgtk-3-bin libgtk-3-dev libwebkit2gtk-3.0-dev \
+ valac rsync libjson-glib-dev libarchive-dev libnotify-dev libunity-dev \
+ gdb libtool gdbserver geany libgstreamer1.0-dev gstreamer1.0-plugins-base \
+ gstreamer1.0-plugins-good gstreamer1.0-fluendo-mp3 gstreamer1.0-pulseaudio
