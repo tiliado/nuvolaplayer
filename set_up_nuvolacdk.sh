4f8df5d39291caaa24d34f32127a24e4bb6d0b6e
Jiří Janoušek <janousek.jiri@gmail.com>
2017-01-28 11:59:20 +0100

    Add script to set up the Nuvola CDK environment
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

---
+++ set_up_nuvolacdk.sh
@@ -0,0 +1,13 @@
+if [ -z "$DIORITE_PATH" ]; then DIORITE_PATH="$1"; fi
+
+if [ -z "$DIORITE_PATH" -o ! -d "$DIORITE_PATH" ]; then
+	echo "Error: Specify the path to the Diorite library repository checkout as the first argument"
+else
+	. set_up_env.sh
+	export DIORITE_PATH="$DIORITE_PATH"
+	export PKG_CONFIG_PATH="$DIORITE_PATH/build"
+	export VAPIDIR="$DIORITE_PATH/build"
+	export C_INCLUDE_PATH="$DIORITE_PATH/build"
+	export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$DIORITE_PATH/build"
+	export LIBRARY_PATH="$DIORITE_PATH/build"
+fi
