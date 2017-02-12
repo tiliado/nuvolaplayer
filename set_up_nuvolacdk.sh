bb1d6d974bca673af3a4f9d7741b8e8ecf132d2e
Jiří Janoušek <janousek.jiri@gmail.com>
2017-02-12 14:58:36 +0100

    Add --cdk --webkitgtk-supports-mse to set_up_nuvolac dk.sh
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- set_up_nuvolacdk.sh
+++ set_up_nuvolacdk.sh
@@ -3,7 +3,7 @@ if [ -z "$DIORITE_PATH" ]; then DIORITE_PATH="$1"; fi
 if [ -z "$DIORITE_PATH" -o ! -d "$DIORITE_PATH" ]; then
 	echo "Error: Specify the path to the Diorite library repository checkout as the first argument"
 else
-	WAF_CONFIGURE=" --flatpak"
+	WAF_CONFIGURE=" --flatpak --cdk --webkitgtk-supports-mse"
 	. set_up_env.sh
 	export DIORITE_PATH="$DIORITE_PATH"
 	export PKG_CONFIG_PATH="$DIORITE_PATH/build"
