125def9891629a2b11e1a46ee26dfd4670d37bf7
Jiří Janoušek <janousek.jiri@gmail.com>
2017-03-31 11:52:26 +0200

    Add /app/lib/pkgconfig to PKG_CONFIG_PATH
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- set_up_nuvolacdk.sh
+++ set_up_nuvolacdk.sh
@@ -6,7 +6,7 @@ else
 	WAF_CONFIGURE=" --flatpak --cdk --webkitgtk-supports-mse --tiliado-oauth2-client-id="
 	. set_up_env.sh
 	export DIORITE_PATH="$DIORITE_PATH"
-	export PKG_CONFIG_PATH="$DIORITE_PATH/build"
+	export PKG_CONFIG_PATH="$DIORITE_PATH/build:/app/lib/pkgconfig:$PKG_CONFIG_PATH"
 	export VAPIDIR="$DIORITE_PATH/build"
 	export C_INCLUDE_PATH="$DIORITE_PATH/build"
 	export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$DIORITE_PATH/build"
