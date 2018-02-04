761eeae36dc420d05bd2532ff3cd76f467172aef
Jiří Janoušek <janousek.jiri@gmail.com>
2018-02-04 18:45:49 +0100

    Remove obsolete electron engine attempt
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- nuvolaruntime/pygi.py
+++
@@ -1,8 +0,0 @@
-def set_up_requirements():
-    import gi
-    gi.require_version("Drt", "1.0")
-    gi.require_version("Drtgtk", "1.0")
-    gi.require_version("Nuvola", "1.0")
-    gi.require_version("Gtk", "3.0")
-    gi.require_version("Gdk", "3.0")
-    gi.require_version("GdkX11", "3.0")
--- nuvolaruntime/webengine/electron/__init__.py
+++
@@ -1,9 +0,0 @@
-from .options import ElectronOptions
-from .thread import ElectronThread
-from .engine import ElectronEngine
-
-
-def setup_electron():
-    from gi.repository import Nuvola
-    Nuvola.WebOptions.set_default(ElectronOptions)
-
